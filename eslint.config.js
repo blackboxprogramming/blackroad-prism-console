b4e59722efe4a7d2859d1b1be17069a9e3133a3e
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-08-20 22:58:38 -0500

    Merge commit '3abe9a905f8525c059d84a9093a75e5c212beb48'

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,18 @@
 import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
+module.exports = [
+  {
+    ignores: ["node_modules/**"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+    },
+    rules: {},
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 
 export default [
   {
