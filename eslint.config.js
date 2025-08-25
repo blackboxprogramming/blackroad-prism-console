760a22cdb9595bc1e907f33b055f874e1e2f9afe
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-08-24 21:11:24 -0500

    chore: use flat eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,22 @@
-import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
-export default [ js.configs.recommended, prettier, {
-  ignores: ["node_modules/","dist/","build/",".github/","public/vendor/"],
-  rules: { "no-unused-vars":["warn",{ "argsIgnorePattern":"^_", "varsIgnorePattern":"^_" }], "no-undef":"warn" }
-} ];
+module.exports = [
+  {
+    ignores: ['node_modules/**','dist/**','build/**','.github/**','public/vendor/**','var/**']
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        process: 'readonly',
+        setTimeout: 'readonly'
+      }
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'no-undef': 'warn'
+    }
+  }
+];
