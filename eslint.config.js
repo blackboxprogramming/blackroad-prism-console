9a97b02648694de714dfddb4f96add1d1f5307df
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-09-01 23:01:56 -0500

    fix: repair cleanup bot and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,23 @@
-import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
-export default [ js.configs.recommended, prettier, {
-  ignores: ["node_modules/","dist/","build/",".github/","public/vendor/"],
-  rules: { "no-unused-vars":["warn",{ "argsIgnorePattern":"^_", "varsIgnorePattern":"^_" }], "no-undef":"warn" }
-} ];
+const js = require("@eslint/js");
+const prettier = require("eslint-config-prettier");
+
+module.exports = [
+  js.configs.recommended,
+  prettier,
+  {
+    ignores: [
+      "node_modules/",
+      "dist/",
+      "build/",
+      ".github/",
+      "public/vendor/",
+    ],
+    rules: {
+      "no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "no-undef": "warn",
+    },
+  },
+];
