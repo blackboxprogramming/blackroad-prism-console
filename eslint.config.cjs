240c520f0927c3341167151121c1090eef2e62ef
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-09-01 23:23:41 -0500

    chore: use commonjs eslint config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,6 +1,11 @@
 module.exports = [
   {
-    files: ['**/*.{js,mjs,cjs}'],
-    languageOptions: { ecmaVersion: 2022, sourceType: 'module' },
-  },
+    files: ["**/*.{js,mjs,cjs,jsx}",],
+    ignores: ["node_modules/","dist/","build/",".github/","public/vendor/"],
+    languageOptions: { ecmaVersion: 2021, sourceType: "module" },
+    rules: {
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }],
+      "no-undef": "warn"
+    }
+  }
 ];
