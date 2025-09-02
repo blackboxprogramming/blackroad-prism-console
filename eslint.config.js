d0a7f08906c2d7d7fa5b7efd1b73c6467131e2ed
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-09-01 23:22:59 -0500

    chore: remove esm eslint config

--- eslint.config.js
+++
@@ -1,6 +0,0 @@
-import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
-export default [ js.configs.recommended, prettier, {
-  ignores: ["node_modules/","dist/","build/",".github/","public/vendor/"],
-  rules: { "no-unused-vars":["warn",{ "argsIgnorePattern":"^_", "varsIgnorePattern":"^_" }], "no-undef":"warn" }
-} ];
