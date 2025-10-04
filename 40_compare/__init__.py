05ce58c5d40f7340b184e4262de4f9a5c924002a
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-10-04 15:12:46 -0500

    Add universal sim starter IO helpers and docs

---
+++ 40_compare/__init__.py
@@ -0,0 +1,9 @@
+"""Utility helpers for comparing simulation outputs."""
+
+from .io_utils import load_point_cloud_ply, load_npz_scalar_field, load_npy
+
+__all__ = [
+    "load_point_cloud_ply",
+    "load_npz_scalar_field",
+    "load_npy",
+]
