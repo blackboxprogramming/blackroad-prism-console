95bd550196e05075f7994828c394dd4f59db8f77
blackboxprogramming <118287761+blackboxprogramming@users.noreply.github.com>
2025-10-05 12:47:51 -0500

    feat: add ecs canary deployment support (#1949)

--- br-infra-iac/modules/ecs-service-alb/outputs.tf
+++ br-infra-iac/modules/ecs-service-alb/outputs.tf
@@ -9,3 +9,19 @@ output "service_name" {
 output "https_url" {
   value = "https://${var.domain_name}"
 }
+
+output "tg_blue_arn" {
+  value = aws_lb_target_group.tg.arn
+}
+
+output "tg_green_arn" {
+  value = aws_lb_target_group.tg_canary.arn
+}
+
+output "https_listener_arn" {
+  value = aws_lb_listener.https.arn
+}
+
+output "alb_arn_suffix" {
+  value = aws_lb.this.arn_suffix
+}
